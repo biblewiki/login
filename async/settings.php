d452581311b4ac2d762ad0f2b76391ca238cb0c1
Joel Kohler <47793291+joelkohler@users.noreply.github.com>
2019-08-10 17:44:19 +0200

    Change edit host

--- async/settings.php
+++ async/settings.php
@@ -3,7 +3,7 @@
 define(HOST_DOMAIN, "biblewiki.one");
 
 // Edit page address
-define(EDIT_HOST, "https://edit.joel.biblewiki.one");
+define(EDIT_HOST, "https://edit.biblewiki.one");
 
 // Login page address
 define(LOGIN_HOST, "https://login.biblewiki.one");
